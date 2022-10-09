3f55b2993664c93b97727d8feeb93630db1cf6c8
mk2000 <manpreetkaur06953@gmail.com>
2022-10-09 15:18:42 +0530

    fetch Users

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "../styles/App.css";
+import { useState, useEffect } from "react";
 const App = () => {
-//code here 
- 
+  const [name, setName] = useState("");
+  const [id, setId] = useState(1);
 
+  useEffect(() => {
+    fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => setName(data.name))
+      .catch((err) => console.log(err));
+  }, [id]);
 
-
+  const changeInput = (e) => {
+    setId(e.target.value);
+  };
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" onChange={changeInput} />
+      <input id="input" onChange={(e) => changeInput(e)} />
       <p id="name">{name}</p>
     </div>
   );
-}
-
+};
 
 export default App;
